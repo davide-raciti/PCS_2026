7ffbd274fd1a109a2a080ec1132994715b8e078a
Davide Raciti <Davide Raciti>
2026-03-12 17:50:12 +0000

    Caricamento cartella esercitazione1

---
+++ esercitazione1/medie.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+# include <fstream>
+using namespace std;
+
+int main(int argc, const char *argv[]) { // argc numero input, argv vettore input
+
+    string filename = argv[1];
+
+    ifstream ifs(filename); // legge il testo che gli stai passando
+    if ( ifs.is_open() ) {
+        string location;
+        double temp1, temp2, temp3, temp4;
+        while(ifs >> location >> temp1 >> temp2 >> temp3 >> temp4) {
+            double media = (temp1 + temp2 + temp3 + temp4) / 4;
+            cout << location << " " << media << "\n";
+        }
+    }
+    else {
+        cerr << "Errore nell'apertura del file\n";
+        return 1;
+    }
+
+    return 0;
+}
